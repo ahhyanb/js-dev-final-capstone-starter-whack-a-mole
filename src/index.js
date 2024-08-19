1781c639d5cc7f12f5f6c642367c115467b130c5
Ryan Bartolome <rahhyahh@icloud.com>
2024-08-18 22:49:25 -1000

    updated setDelay()

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ let difficulty = "hard";
  *
  */
 function randomInteger(min, max) {
-  // return Math.floor(Math.random() * (max - min + 1)) + min;
+   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -40,8 +40,15 @@ function randomInteger(min, max) {
  *
  */
 function setDelay(difficulty) {
-  // TODO: Write your code here.
-  
+  if (difficulty === "easy") {
+    return 1500;
+  }
+  if (difficulty === "normal") {
+    return 1000;
+  }
+  else {
+    return randomInteger(600, 1200);
+  }
 }
 
 /**
